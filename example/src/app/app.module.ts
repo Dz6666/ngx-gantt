60393c9d37d0d4b87aa1e8ed3558f3eefe492584
zhangwen <1062680993@qq.com>
2021-06-09 15:31:22 +0800

    chore: bump docgeni 1.0.2

--- example/src/app/app.module.ts
+++ example/src/app/app.module.ts
@@ -7,10 +7,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { CommonModule } from '@angular/common';
 import { AppGanttFlatExampleComponent } from './gantt-flat/flat.component';
 import { AppGanttRangeExampleComponent } from './gantt-range/gantt-range.component';
-import { EXAMPLE_MODULES, DOCGENI_SITE_PROVIDERS } from './content/index';
+import { DOCGENI_SITE_PROVIDERS } from './content/index';
 import { DocgeniTemplateModule } from '@docgeni/template';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppGanttFlatComponent } from './gantt-flat/component/flat.component';
+import { EXAMPLE_MODULES } from './content/example-modules';
 
 @NgModule({
     declarations: [
